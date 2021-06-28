c0a8826bb7ad59d5c5d5001ce3ff8c57005ee8d8
SergeitZ <sergiofranco6@gmail.com>
2021-06-28 19:59:01 -0400

    added test sout print

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello user");
+        System.out.println("Hello user2");
 
         ArrayList<Animal> animals = new ArrayList<>();
 
