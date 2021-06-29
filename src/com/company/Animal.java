59c1767252f5e5e3f4df7629d4dd25152e65b7b5
SergeitZ <sergiofranco6@gmail.com>
2021-06-28 20:56:14 -0400

    Tweaked Animal Class

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -2,11 +2,11 @@ package com.company;
 
 public class Animal {
 
-    int legs = 4;
-    String size;
-    boolean isFixed;
+    public int legs = 4;
+    public String size;
+    public boolean isFixed;
     public String name;
-    double weight;
+    public double weight;
 
     public Animal (int legs, String size, String name, double weight, boolean isFixed) {
         this.legs = legs;
